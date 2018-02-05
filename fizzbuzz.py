dc3f7d98a8ed7c127ed245578f1004af4bcced27
Dominque Cox <dominquecox@dominques-air.attlocal.net>
2018-02-05 16:15:44 -0500

    first python commit

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizz_buzz(num):
+    if num % 10 == 0: 
+        print("FizzBuzz")
+    elif num % 5 == 0: 
+        print("Buzz")
+    elif num % 3 == 0: 
+        print("Fizz")
+    else: 
+        print(num)
+
+for i in range(1,101):
+    fizz_buzz(i)
